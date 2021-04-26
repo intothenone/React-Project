9fc557581073104769a9b475c1d83f5cf485f638
Cesar Augusto <c-castle@hotmail.com>
2021-04-26 15:12:26 -0300

    Implementar component Promotion Card

---
+++ src/components/Promotion/Card/Card.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Card.css'
+
+const PromotionCard = ({promotion}) => (
+    <div className="promotion-card">
+        <img src={promotion.imageUrl} className="promotion-card__image"/>
+        <div className="promotion-card__info">
+            <h1 className="promotion-card__title">{promotion.title}</h1>
+            <span className="promotion-card__price">R$ {promotion.price}</span>
+            <footer className="promotion-card__footer">
+                {promotion.comments.length > 0 && (
+                    <div className="promotion-card__comment">"{promotion.comments[0].comment}"</div>
+                )}
+                <div className="promotion-card__comments-count">{promotion.comments.length}{' '} Comentário{promotion.comments.length > 1 ? 's' : ''}</div>
+                <a href={promotion.url} target="_blank" className="promotion-card__link">Ir para o site</a>
+            </footer>
+        </div>
+    </div>
+)
+
+export default PromotionCard
